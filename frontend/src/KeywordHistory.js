b36f2b189742ffa68f0a328c0fcdc058300f5790
Minsoek96 <FE.minseok96@gmail.com>
2023-07-26 21:08:37 +0900

    새로고침시 데이터 유지 기능 추가

--- frontend/src/KeywordHistory.js
+++ frontend/src/KeywordHistory.js
@@ -1,3 +1,6 @@
+import store from "./store.js";
+import uniqueKey from "./utils/uniqueKey.js";
+
 class KeywordHistory {
   data = {
     show: true,
@@ -6,7 +9,7 @@ class KeywordHistory {
   constructor({ $target, onSearch }) {
     const $KeyWordHistory = document.createElement("ul");
     this.$KeyWordHistory = $KeyWordHistory;
-    this.$KeyWordHistory.className = "KeyWordHistory"
+    this.$KeyWordHistory.className = "KeyWordHistory";
     $target.append(this.$KeyWordHistory);
     this.initHistory();
     this.$KeyWordHistory.addEventListener("click", (e) => {
@@ -17,12 +20,14 @@ class KeywordHistory {
   }
 
   initHistory() {
+    let history = store.getStorage("history");
+    history = history && history.splice(0, 5);
+    history = uniqueKey(history);
     this.setState({
       show: !this.data.show,
-      history: store.getStorage("history")
-        ? store.getStorage("history")
-        : store.setStorage("history", []),
+      history: history ? history : store.setStorage("history", []),
     });
+    console.log(this.data.history)
   }
 
   setState(nextData) {
@@ -36,8 +41,8 @@ class KeywordHistory {
     this.data.show
       ? (this.$KeyWordHistory.innerHTML = this.data.history
           .map((keyword) => `<li class="KeyWordHistory_Item">${keyword}</li>`)
-          .splice(0, 5)
           .join(""))
       : (this.$KeyWordHistory.innerHTML = "");
   }
 }
+export default KeywordHistory;
